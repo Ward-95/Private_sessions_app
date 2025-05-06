1d7876e6495f4360ff5b54c39284bd88bc52e69d
Ward-95 <wardfaraj07@gmail.com>
2025-05-06 12:42:28 +0300

    Create service_worker.js

---
+++ service_worker.js
@@ -0,0 +1,20 @@
+const cacheName = "sessions-cache-v1";
+const assets = [
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json",
+  "icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
